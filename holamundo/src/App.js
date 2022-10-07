b51301b861bca0b85b946be7eeac5ac405b630b9
Elbert <ddavid0116@gmail.com>
2022-10-07 00:09:27 -0500

    Segundo commit

--- holamundo/src/App.js
+++ holamundo/src/App.js
@@ -1,13 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+       
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hola mundo
         </p>
         <a
           className="App-link"
@@ -15,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hola Mundo!
         </a>
       </header>
     </div>
